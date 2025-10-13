a2aa588e478a2b2bc223893d2d945a432880eea1
zeemeedx <kaua123848@gmail.com>
2025-10-13 14:23:58 -0300

    implementa a classe usuario

---
+++ src/main/java/com/application/javai/Usuario.java
@@ -0,0 +1,17 @@
+package main.java.com.application.javai;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+
+    // Getters
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+
+    // Setters
+    public void setId (int id) { this.id = id; }
+    public void setNome (String nome) { this.nome = nome; }
+    public void setEmail (String email) { this.email = email; }
+}
