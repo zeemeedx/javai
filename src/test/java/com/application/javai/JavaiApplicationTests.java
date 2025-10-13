9bba4a00470a949089fea4663f76009da22a64e8
zeemeedx <kaua123848@gmail.com>
2025-10-13 11:48:43 -0300

    adiciona os arquivos do projeto

---
+++ src/test/java/com/application/javai/JavaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.javai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
