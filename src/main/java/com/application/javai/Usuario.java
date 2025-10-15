1bd1eeabe288bdab2b9cfe67ec166478a1659ea9
Seibel <jpseibelcervo@yahoo.com.br>
2025-10-15 10:35:22 -0300

    path do package e constutor Usuario

--- src/main/java/com/application/javai/Usuario.java
+++ src/main/java/com/application/javai/Usuario.java
@@ -1,9 +1,15 @@
-package main.java.com.application.javai;
+package com.application.javai;
 
 public class Usuario {
     private int id;
     private String nome;
     private String email;
+
+    public Usuario(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
 
     // Getters
     public int getId() { return id; }
